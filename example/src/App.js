dd4b885c518ea17ff74f941a138d916e75197dd4
dilarademirhan <dilara.demirhan@stu.fsm.edu.tr>
2023-04-16 03:29:38 +0300

    Initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-import { ExampleComponent } from 'dlr-button-ui'
+import { Button } from 'dlr-button-ui'
 import 'dlr-button-ui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Button type='.primary'/>
+      <Button type='.default'/>
+      <Button type='.dashed'/>
+      <Button type='.text'/>
+      <Button type='.link'/>
+    </>
+    );
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = ({ type }) => {
+  let style;
+  let text  = type.substring(1) + ' Button';
+  if (type == '.primary') {
+    style = styles.primary;
+  } else if (type == '.dashed') {
+    style = styles.dashed;
+  } else if (type == '.text') {
+    style = styles.text;
+  } else if (type == '.link') {
+    style = styles.link;
+  } else {
+    style = '';
+  }
+  return <button className={ `${ styles.btn } ${ style }`}> { text } </button>
 }
